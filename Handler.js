e8c2a1a31948bec654e4e61e9643815281d8761d
KneeShoe <KneeShoe@users.noreply.github.com>
2020-05-28 22:41:32 +0530

    bubble sort try 1

--- Handler.js
+++ Handler.js
@@ -5,7 +5,7 @@ var boxes=[];
 	 output.innerHTML = size.value;
  }
 
- function genarr(){
+ function genarrrand(){
 	 document.getElementById("gfg").innerHTML="";
 	 var size=document.getElementById("Size").value;
 	 var width = ((1147-3*(size-1)) / size);
@@ -13,8 +13,34 @@ var boxes=[];
 	 var i=0;
 	 for(i=0;i<size;i++){
 		 boxes[i]=Math.floor(Math.random() * 200) + 25;
-//		document.write("<div style= \"width:"+width+"; height:200px; position:absolute; top:110px; left:"+left+"; background:rgb(255, 220, 106);\"></div>");
-		 document.getElementById("gfg").innerHTML+="<div style= \"width:"+width+"; height:"+boxes[i]+"px; position:absolute; top:110px; left:"+left+"px; background:rgb(255, 220, 106);\"></div>";
+		 document.getElementById("gfg").innerHTML+="<div id="+i+" style= \"width:"+width+"; height:"+boxes[i]+"px; position:absolute; top:110px; left:"+left+"px; background:rgb(255, 220, 106);\"></div>";
 		 left=parseInt(left)+parseInt(width)+3;
+	 }
+ }
+
+function sleep(milliseconds) {
+	var date = Date.now();
+	var currentDate = null;
+	do {
+		currentDate = Date.now();
+	} while (currentDate - date < milliseconds);
+}
+
+ function bubblesort() {
+	 var i,j,temp;
+	 var n=boxes.length;
+	 for(i=0;i<n;i++){
+	 	document.getElementById(i).style.backgroundColor="blue";
+			 for(j=0;j<n-i-1;j++){
+				 document.getElementById(j).style.backgroundColor="red";
+				 if(boxes[j]>boxes[j+1]){
+					 temp=boxes[j+1];
+					 boxes[j+1]=boxes[j];
+					 boxes[j]=temp;
+					 document.getElementById(j).style.height=boxes[j];
+					 document.getElementById(j+1).style.height=boxes[j+1];
+					 
+				 }
+			 }
 	 }
  }
