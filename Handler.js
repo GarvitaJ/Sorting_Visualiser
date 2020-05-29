deb73242902cfdbbd733442c493e7d73ffe1649d
GarvitaJ <garvitajhawar@gmail.com>
2020-05-29 16:16:29 +0530

    Gen full array bubble sort

--- Handler.js
+++ Handler.js
@@ -18,29 +18,45 @@ var boxes=[];
 	 }
  }
 
-function sleep(milliseconds) {
-	var date = Date.now();
-	var currentDate = null;
-	do {
-		currentDate = Date.now();
-	} while (currentDate - date < milliseconds);
+// function sleep(milliseconds) {
+// 	var date = Date.now();
+// 	var currentDate = null;
+// 	do {
+// 		currentDate = Date.now();
+// 	} while (currentDate - date < milliseconds);
+// }
+
+function showarr(size){
+	document.getElementById("gfg").innerHTML="";
+	var width = ((1147-3*(size-1)) / size);
+	var left= 70;
+	var i=0;
+	for(i=0;i<size;i++){
+		document.getElementById("gfg").innerHTML+="<div id="+i+" style= \"width:"+width+"; height:"+boxes[i]+"px; position:absolute; top:110px; left:"+left+"px; background:rgb(255, 220, 106);\"></div>";
+		left=parseInt(left)+parseInt(width)+3;
+	}
 }
 
  function bubblesort() {
-	 var i,j,temp;
+	 var temp;
 	 var n=boxes.length;
-	 for(i=0;i<n;i++){
-	 	document.getElementById(i).style.backgroundColor="blue";
-			 for(j=0;j<n-i-1;j++){
-				 document.getElementById(j).style.backgroundColor="red";
-				 if(boxes[j]>boxes[j+1]){
-					 temp=boxes[j+1];
-					 boxes[j+1]=boxes[j];
-					 boxes[j]=temp;
-					 document.getElementById(j).style.height=boxes[j];
-					 document.getElementById(j+1).style.height=boxes[j+1];
-					 
-				 }
+	 setTimeout(function(){
+	 	for(let i=0;i<n;i++){
+			document.getElementById(i).style.backgroundColor="blue";
+			 for(let j=0;j<n-i-1;j++){
+			 	setTimeout(function(){
+					document.getElementById(j).style.backgroundColor="red";
+					if(boxes[j]>boxes[j+1]){
+						setTimeout(function(){
+							temp=boxes[j+1];
+							boxes[j+1]=boxes[j];
+							boxes[j]=temp;
+							showarr(n);
+						},10);
+
+					}
+				},100);
 			 }
-	 }
+		 }
+	 }, 1000);
  }
