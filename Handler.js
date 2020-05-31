5a9d0efe9aec28bd80c17ec251b63f99e405d6a1
GarvitaJ <garvitajhawar@gmail.com>
2020-05-31 19:50:27 +0530

    merge sort working

--- Handler.js
+++ Handler.js
@@ -1,4 +1,4 @@
-var boxes=[];
+var boxes=[],temp=[];
  function setarrsize(){
 	 var size=document.getElementById("Range");
 	 var output=document.getElementById("Size");
@@ -117,3 +117,79 @@ async function selectionsort() {
 };
 
 
+function showarrmerge(i,j){
+	document.getElementById("gfg").innerHTML="";
+	var size=boxes.length;
+	var width = ((1147-3*(size-1)) / size);
+	var left= 70;
+	var k=0;
+	for(k=0;k<size;k++){
+		// if(k==min)
+		// 	document.getElementById("gfg").innerHTML+="<div id="+k+" style= \"width:"+width+"; height:"+boxes[k]+"px; position:absolute; top:110px; left:"+left+"px; background-color:blue;\"></div>";
+		// else if(k<=i)
+		// 	document.getElementById("gfg").innerHTML+="<div id="+k+" style= \"width:"+width+"; height:"+boxes[k]+"px; position:absolute; top:110px; left:"+left+"px; background-color:red;\"></div>";
+		// else
+		if(k<i+j)
+			document.getElementById("gfg").innerHTML+="<div id="+k+" style= \"width:"+width+"; height:"+temp[k]+"px; position:absolute; top:110px; left:"+left+"px; background-color:red;\"></div>";
+		else
+			document.getElementById("gfg").innerHTML+="<div id="+k+" style= \"width:"+width+"; height:"+boxes[k]+"px; position:absolute; top:110px; left:"+left+"px; background-color:rgb(255, 220, 106);\"></div>";
+
+		left=parseInt(left)+parseInt(width)+3;
+	}
+
+}
+
+function merge(boxes,s,m,e){
+	var t=speed();
+	var k=0;
+	var i=s,j=m+1;
+	while(i<=m && j<=e)
+	{
+		if(boxes[i]<=boxes[j]){
+			temp[k++]=boxes[i++];
+		}
+		else
+		{
+			temp[k++]=boxes[j++];
+		}
+
+		// var promise= new Promise((resolve, reject) =>
+		// 	setTimeout(function(){
+		//
+		// 	},t)
+		// );
+		// await promise;
+
+		// showarrmerge(i,j);
+	}
+	while(i<=m)
+		temp[k++]=boxes[i++];
+	while(j<=e)
+		temp[k++]=boxes[j++];
+	for(var p = s; p <= e; p+= 1) {
+		boxes[p] = temp[p - s]
+	}
+}
+
+function mergesrt(boxes,s,e) {
+	if(s<e){
+		var m=Math.floor((s+e)/2);
+		mergesrt(boxes,s,m);
+		mergesrt(boxes,m+1,e);
+		merge(boxes,s,m,e);
+	}
+
+}
+
+function mergesort() {
+	var s=0,e=boxes.length-1;
+	document.getElementById("gfg").innerHTML="";
+	for(var i=0;i<boxes.length;i++){
+		document.getElementById("gfg").innerHTML+=boxes[i]+" ";
+	}
+	mergesrt(boxes,s,e);
+	document.getElementById("gfg").innerHTML+="\n";
+	for(var i=0;i<boxes.length;i++){
+		document.getElementById("gfg").innerHTML+=boxes[i]+" ";
+	}
+}
